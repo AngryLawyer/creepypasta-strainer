aa4db17ad2cff5a916109078a2cd655951c928b1
Tony Aldridge <tony@dabapps.com>
2016-01-07 15:26:15 +0000

    Added reminders as to where in the API to go

--- src/strainer.ml
+++ src/strainer.ml
@@ -1,4 +1,8 @@
 open Core.Std
 
+(* endpoints:
+ * http://creepypasta.wikia.com/api/v1/Articles/List?category=PotM&limit=1000
+ * http://creepypasta.wikia.com/api/v1/Articles/List?category=Spotlighted_Pastas&limit=1000
+ *)
 let () =
     ()
