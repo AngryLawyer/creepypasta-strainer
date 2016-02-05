8d1055c81a1ee91d0f8e6748fb4f3a6945bc252c
Tony Aldridge <zaragopha@hotmail.com>
2016-02-05 18:04:16 +0000

    Case sensitivity

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-corebuild -I src -pkg async,cohttp.async,yojson,textwrap,sqlite3ez src/strainer.byte
-corebuild -I src -pkg sqlite3ez src/init_db.byte
+corebuild -I src -pkg async,cohttp.async,yojson,textwrap,sqlite3EZ src/strainer.byte
+corebuild -I src -pkg sqlite3EZ src/init_db.byte
